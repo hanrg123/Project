f1443e899070aabab110be25276742a2c6547caa
ehqls <ehqls@192.168.123.12>
2019-03-29 14:58:35 +0900

    MemberVO

---
+++ Project/src/main/java/com/spring/biz/vo/MemberVO.java
@@ -0,0 +1,57 @@
+package com.spring.biz.vo;
+
+import java.sql.Date;
+
+public class MemberVO {
+	private int memberCode;
+	private String memberName;
+	private int memberPhone;
+	private String memberAddr;
+	private String memberPass;
+	private Date memberBirthday;
+	private String memberId;
+	
+	
+	public int getMemberCode() {
+		return memberCode;
+	}
+	public void setMemberCode(int memberCode) {
+		this.memberCode = memberCode;
+	}
+	public String getMemberName() {
+		return memberName;
+	}
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	public int getMemberPhone() {
+		return memberPhone;
+	}
+	public void setMemberPhone(int memberPhone) {
+		this.memberPhone = memberPhone;
+	}
+	public String getMemberAddr() {
+		return memberAddr;
+	}
+	public void setMemberAddr(String memberAddr) {
+		this.memberAddr = memberAddr;
+	}
+	public String getMemberPass() {
+		return memberPass;
+	}
+	public void setMemberPass(String memberPass) {
+		this.memberPass = memberPass;
+	}
+	public Date getMemberBirthday() {
+		return memberBirthday;
+	}
+	public void setMemberBirthday(Date memberBirthday) {
+		this.memberBirthday = memberBirthday;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+}
